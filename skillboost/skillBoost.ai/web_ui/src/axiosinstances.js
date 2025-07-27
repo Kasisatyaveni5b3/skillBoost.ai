03202989fdb1a7b9dd1e72f3afc81f88c8037735
kasisatyaveni5b3 <kasisatyaveninukella@gmail.com>
2025-07-27 21:52:34 +0530

    changed the production url

--- skillboost/skillBoost.ai/web_ui/src/axiosinstances.js
+++ skillboost/skillBoost.ai/web_ui/src/axiosinstances.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL:import.meta.REACT_APP_API_URL,
+    baseURL:import.meta.env.REACT_APP_API_URL,
    headers: {
     'Content-Type': 'application/json',
   },
